f8a28da4cc5426ec4b6edaf05ba50d55b9f920c8
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2024-12-30 20:08:47 -0800

    Create runeems.sh

---
+++ EEMS/runeems.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#run with 200 demes
+../eems/runeems_snps/src/runeems_snps --params params.200demes.2  --seed 111
+
+#run with 400 demes
+../eems/runeems_snps/src/runeems_snps --params params.400demes.2  --seed 111
+
+#run with 800 demes
+../eems/runeems_snps/src/runeems_snps --params params.800demes.2  --seed 111
+
+
+
+
