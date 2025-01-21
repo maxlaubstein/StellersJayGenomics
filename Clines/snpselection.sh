0406e92eded36c88e8884913fe752d5674ce5227
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2025-01-20 19:20:26 -0800

    Update snpselection.sh

--- Clines/snpselection.sh
+++ Clines/snpselection.sh
@@ -1,5 +1,9 @@
 #We want to get sites that are polymorphic within the core contact zone (pops 83, 84, 85)
-vcftools --vcf pops80-89_LD_Pruned.recode.vcf --keep pops83-84-85 --freq --out pops83-84-85_SNPfreqs
+#Get sites with MAF greater than 0.3 and 0.7
+vcftools --vcf pops80-89_LD_Pruned.recode.vcf --keep pops83-84-85 --maf 0.3 --out pops83-84-85_0.3MAF --recode
 
-#Get a list of all SNPs with MAF frequency between 0.3 and 0.7 within the core contact zone
-awk '$5 >= 0.3 && $5 <= 0.7 {print $1 ":" $2}' pops83-84-85_SNPfreqs.frqs > clineSNPs.txt
+#Get the the site coords
+awk 'BEGIN {OFS="\t"} !/^#/ {print $1 "  " $2}' pops83-84-85_0.3MAF.recode.vcf > clineSNPs.txt
+
+#Keep only these sites in our vcf with samples from across the whole contact zone transect (pops 80 - 89)
+vcftools --vcf pops80-89_LD_Pruned.recode.vcf --positions clineSNPs.txt --out pops80-89_ClineSNPs --recode
