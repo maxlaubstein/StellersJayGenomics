4391d41e644c65a912077f9ae6078561ff572cce
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2025-03-29 21:52:28 -0700

    Update RunADMX_nomesoamerica.sh

--- admixture/RunADMX_nomesoamerica.sh
+++ admixture/RunADMX_nomesoamerica.sh
@@ -1,2 +1,4 @@
-cd /media/data2/STJARangewideGenomics/admixture_analyses
+cd /media/data2/STJARangewideGenomics/admixture_analyses/nomeso
+plink --vcf ../VCFdata/41-Cyanocitta_annotated_pruned_0.6.vcf --make-bed --allow-extra-chr 
+
 vcftools --vcf ../VCFdata/41-Cyanocitta_annotated_pruned_0.6.vcf --make-bed --allow-extra-chr --remove-indv MVZ188144 --remove-indv MVZ189266 --remove-indv MVZ189267 --remove-indv UWBM106814 --out nomesoamerica
