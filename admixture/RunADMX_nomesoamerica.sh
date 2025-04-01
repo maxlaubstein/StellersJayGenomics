c9715ca486e554d7a352eaf3d452a9f3bde4f0bd
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2025-04-01 15:59:50 -0700

    Update RunADMX_nomesoamerica.sh

--- admixture/RunADMX_nomesoamerica.sh
+++ admixture/RunADMX_nomesoamerica.sh
@@ -6,4 +6,4 @@ cd /media/data2/STJARangewideGenomics/admixture_analyses/nomeso
 plink --vcf ../../VCFdata/autosomal_annotated_pruned_nomeso.recode.vcf --make-bed --out autosomal_annotated_pruned_nomeso --allow-extra-chr 
 plink --bfile autosomal_annotated_pruned_nomeso --recode12 --out autosomal_annotated_pruned_nomeso --allow-extra-chr
 
-for K in 2 3 4 5 6 7 8 9 ; do admixture --cv autosomal_annotated_pruned_nomeso.ped $K | tee ADMX.all.log${K}.out; done
+for K in 2 3 4 5 6 7 8 9 ; do admixture --cv autosomal_annotated_pruned_nomeso.ped $K | tee ADMX.nomeso.log${K}.out; done
