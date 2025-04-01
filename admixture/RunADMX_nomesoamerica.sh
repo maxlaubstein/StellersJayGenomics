fe2de2008fa5e49bb8c10d570710b3b683a4d7b5
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2025-04-01 14:59:45 -0700

    Update RunADMX_nomesoamerica.sh

--- admixture/RunADMX_nomesoamerica.sh
+++ admixture/RunADMX_nomesoamerica.sh
@@ -1,4 +1,9 @@
-cd /media/data2/STJARangewideGenomics/admixture_analyses/nomeso
-plink --vcf ../VCFdata/41-Cyanocitta_annotated_pruned_0.6.vcf --make-bed --allow-extra-chr 
+#!/bin/bash
+cd /media/data2/STJARangewideGenomics/VCFdata
+vcftools --vcf autosomal_annotated_pruned.recode.vcf  --remove ../admixture_analyses/nomeso/meso --recode --out autosomal_annotated_pruned_nomeso
 
-vcftools --vcf ../VCFdata/41-Cyanocitta_annotated_pruned_0.6.vcf --make-bed --allow-extra-chr --remove-indv MVZ188144 --remove-indv MVZ189266 --remove-indv MVZ189267 --remove-indv UWBM106814 --out nomesoamerica
+cd /media/data2/STJARangewideGenomics/admixture_analyses/nomeso
+plink --vcf ../../VCFdata/autosomal_annotated_pruned_nomeso.recode.vcf --make-bed --out autosomal_annotated_pruned_nomeso --allow-extra-chr 
+plink --bfile autosomal_annotated_pruned_nomeso --recode12 --out autosomal_annotated_pruned_nomeso --allow-extra-chr
+
+for K in 2 3 4 5 6 7 8 9 ; do admixture --cv autosomal_annotated_pruned_nomeso.ped $K | tee ADMX.all.log${K}.out; done
