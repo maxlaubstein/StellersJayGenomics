114ebf251733d5684f26390c46b42e6190d5c226
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2025-04-01 13:23:01 -0700

    Create RunADMX.sh

---
+++ admixture/RunADMX.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /media/data2/STJARangewideGenomics/VCFdata
+vcftools --vcf 41-Cyanocitta_annotated_pruned_0.6.vcf  --not-chr JANXIP010000003.1 --not-chr JANXIP010000028.1 --not-chr JANXIP010000352.1 --not-chr JANXIP010000079.1 --recode --out autosomal_annotated_pruned
+
+cd /media/data2/STJARangewideGenomics/admixture_analyses/allsamples
+plink --vcf ../../VCFdata/autosomal_annotated_pruned.recode.vcf --make-bed --allow-extra-chr 
+plink --bfile VCFdata/autosomal_annotated_pruned --recode --out autosomal_annotated_pruned --allow-extra-chr
+
+for K in 2 3 4 5 6 7 8 9 ; do admixture --cv autosomal_annotated_pruned.ped $K | tee ADMXlog${K}.out; done
