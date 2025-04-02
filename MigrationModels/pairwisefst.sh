b6cc37e88be578dc0b7786fc78cbaaa5bf88bbd4
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2025-04-02 16:21:05 -0700

    Update pairwisefst.sh

--- MigrationModels/pairwisefst.sh
+++ MigrationModels/pairwisefst.sh
@@ -12,3 +12,14 @@ vcftools --vcf ../VCFdata/autosomal_annotated_pruned.recode.vcf --weir-fst-pop p
 vcftools --vcf ../VCFdata/autosomal_annotated_pruned.recode.vcf --weir-fst-pop pop85 --weir-fst-pop pop86 --out fst85_86
 vcftools --vcf ../VCFdata/autosomal_annotated_pruned.recode.vcf --weir-fst-pop pop86 --weir-fst-pop pop87 --out fst86_87
 
+awk '{if ($3 < 0) $3=0; print}' fst83_84.weir.fst > tmp && mv tmp fst83_84.weir.fst
+awk '{if ($3 < 0) $3=0; print}' fst84_85.weir.fst > tmp && mv tmp fst84_85.weir.fst
+awk '{if ($3 < 0) $3=0; print}' fst85_86.weir.fst > tmp && mv tmp fst85_86.weir.fst
+awk '{if ($3 < 0) $3=0; print}' fst86_87.weir.fst > tmp && mv tmp fst86_87.weir.fst
+
+fst83_84.weir.fst > tmp && mv tmp fst83_84.weir.fst
+
+awk '!/-nan/'  fst83_84.weir.fst > tmp && mv tmp fst83_84.weir.fst
+awk '!/-nan/'  fst84_85.weir.fst > tmp && mv tmp fst84_85.weir.fst
+awk '!/-nan/'  fst85_86.weir.fst > tmp && mv tmp fst85_86.weir.fst
+awk '!/-nan/'  fst86_87.weir.fst > tmp && mv tmp fst86_87.weir.fst
