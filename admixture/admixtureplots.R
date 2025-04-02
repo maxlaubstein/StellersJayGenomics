bb56a2262518bd335ecca2ab78f1f359cd7aa9ad
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2025-04-01 21:03:44 -0700

    Update admixtureplots.R

--- admixture/admixtureplots.R
+++ admixture/admixtureplots.R
@@ -1,28 +1,18 @@
-setwd("/Users/maxlaubstein/Desktop/STJA")
+setwd("/Users/maxlaubstein/Desktop/STJA/scripts/")
 library(dplyr)
 library(tidyverse)
 library(readxl)
 library(ggh4x)
 library(gridExtra)
 
-
-k2<-"plink_STJA_forAdmix.2.Q"
-k3<-"plink_STJA_forAdmix.3.Q"
-k4<-"plink_STJA_forAdmix.4.Q"
-k5<-"plink_STJA_forAdmix.5.Q"
-k6<-"plink_STJA_forAdmix.6.Q"
-k7<- "plink_STJA_forAdmix.7.Q"
-k8 <- "plink_STJA_forAdmix.8.Q"
-k9 <- "plink_STJA_forAdmix.9.Q"
-
-metadata <- read_excel("1_Cyanocitta_stelleri_WGS_metadata_allsamples_fulldata_v2.xlsx")
+metadata <- read_excel("../1_Cyanocitta_stelleri_WGS_metadata_allsamples_fulldata_v2.xlsx")
 
 metadata$`Pop # rev`[307] <- 103
 metadata$`Pop # rev`[308] <- 104
 metadata$`Pop # rev`[309] <- 105
 metadata$`Pop # rev`[310] <- 106
 
-samplename<-read.table("plink.fam")$V2
+samplename<-read.table("../admixture/plink.fam")$V2
 
 samplepopnum <- data.frame(samplename = samplename, popnumber = rep(NaN, length(samplename)))
 colnames(samplepopnum) <- c("samplename", "popnumber")
@@ -32,8 +22,16 @@ for (i in 1:nrow(samplepopnum)) {
   samplepopnum$popnumber[i] <- as.numeric(subset(metadata, metadata$sample_name == samplepopnum$samplename[i])$`Pop # rev`)
 }
 
-struct_files <- c(k2, k3, k4, k5, k6, k7, k8, k9)
-cat_admx <- do.call("rbind", lapply(struct_files,
+files <- c("../admixture/plink_STJA_forAdmix.2.Q",
+           "../admixture/plink_STJA_forAdmix.3.Q",
+           "../admixture/plink_STJA_forAdmix.4.Q",
+           "../admixture/plink_STJA_forAdmix.5.Q",
+           "../admixture/plink_STJA_forAdmix.6.Q",
+           "../admixture/plink_STJA_forAdmix.7.Q",
+           "../admixture/plink_STJA_forAdmix.8.Q",
+           "../admixture/plink_STJA_forAdmix.9.Q")
+
+cat_admx <- do.call("rbind", lapply(files,
                                     FUN = function(files){
                                       #FUN reads in the .Q file from admixture
                                       #names() extracts the column names from .Q file.  
@@ -55,6 +53,21 @@ cat_admx$k <- as.numeric(cat_admx$k)
 
 cat_admx$pop_sample <- paste(cat_admx$sampleID, "&", cat_admx$popnumber)
 
+
+cat_admx$color <- NA
+for(i in (unique(cat_admx$k))) {
+  dat <- cat_admx[cat_admx$k == unique(cat_admx$k)[1],]
+  for(j in 1:length(unique(dat$popGroup))){
+    if(dat[dat$sampleID=="MVZ181877",]$proportion[j] > 0.9) {dat$color[j] <- 'blue'}
+  }
+}
+
+foo <- cat_admx[cat_admx$sampleID=="MVZCCGP-Cst25_II-E01",]
+foo$popGroup[which(foo$proportion > 0.9)]
+
+
+
+#which popgroup has prop > 0.9 in MVZCCGP-Cst29_II-G01 we designate as 'blue'
 
 
 
@@ -200,3 +213,43 @@ admxplot<-gridExtra::grid.arrange(plotk2, plotk3, plotk4,
                                   bottom = "Sample ID #", 
                                   heights = c(1,1,1,1,1,1,1,2.5), 
                                   right = "K")
+
+k6data <- subset(cat_admx, cat_admx$k==6)
+
+for (i in 1:nrow(k6data)) {
+  if(k6data$popnumber[i] %in% 1:19) {k6data$region[i] <- "N. Coast"}
+  if(k6data$popnumber[i] %in% 20:25) {k6data$region[i] <- "SF Bay"}
+  if(k6data$popnumber[i] %in% 26:30) {k6data$region[i] <- "MTY-SB"}
+  if(k6data$popnumber[i] %in% 31:36) {k6data$region[i] <- "SoCal"}
+  if(k6data$popnumber[i] %in% 37:68) {k6data$region[i] <- "Cascades-Sierra"}
+  if(k6data$popnumber[i] %in% 69:79) {k6data$region[i] <- "Int. NW"}
+  if(k6data$popnumber[i] %in% 80:82) {k6data$region[i] <- "Teton"}
+  if(k6data$popnumber[i] %in% 83:88) {k6data$region[i] <- "Contact"}
+  if(k6data$popnumber[i] %in% 89:102) {k6data$region[i] <- "Rockies"}
+  if(k6data$popnumber[i] %in% 103:106) {k6data$region[i] <- "."}
+}
+
+k6data$regpop <- paste(k6data$sampleID, "&", k6data$region)
+  
+
+# Just k6 plot
+ggplot(aes(x = reorder(regpop, popnumber), y = proportion, fill = factor(popGroup)), data = k6data) +
+  geom_col(aes(fill = factor(popGroup)), linewidth = 0.1) +
+  labs(x=NULL, y = "Ancestry") +
+  scale_y_continuous(expand = c(0,0)) +
+  theme(
+    panel.spacing.x = unit(0.0, "lines"),
+    axis.text.x=element_blank(),
+    axis.text.y=element_blank(),
+    panel.grid = element_blank(),
+    ggh4x.axis.nestline.x = element_line(size = 2),
+    ggh4x.axis.nesttext.x = element_text(size = 11, angle = 0),
+    legend.position = "none",
+    axis.ticks.x = element_blank(),
+    axis.ticks.y = element_blank()
+  ) + 
+  guides(x = ggh4x::guide_axis_nested(delim = "&", check.overlap = F, extend = -0.5), angle = 0)+
+  scale_fill_manual(name = "grp",values = c("#fd7f6f","#bd7ebe","#ffb55a","#b2e061","#7eb0d5","#ffee65"), guide = F) +xlab(NULL)+ylab(NULL)+
+  theme(legend.position = "none")
+
+
