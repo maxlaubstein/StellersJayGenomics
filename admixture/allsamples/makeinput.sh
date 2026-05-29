89f1d9029942a7198e35f947874eb54df3c819a3
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2026-05-29 17:46:26 -0400

    Delete admixture/allsamples directory

--- admixture/allsamples/makeinput.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-vcf="$1"
-
-out="$2"
-
-plink --vcf "$vcf"  --double-id --allow-extra-chr --allow-no-sex --set-missing-var-ids @:#  --make-bed  --out "$out"
-
-plink --bfile "$out" --double-id --allow-extra-chr --allow-no-sex --recode 12 --out "$out"
