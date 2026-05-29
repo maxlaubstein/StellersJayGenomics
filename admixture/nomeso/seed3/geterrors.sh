185966d525e9ab94f2ba27a1b45a99fe90786b44
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2026-05-29 17:26:27 -0400

    Delete admixture/nomeso/seed3/geterrors.sh

--- admixture/nomeso/seed3/geterrors.sh
+++
@@ -1,6 +0,0 @@
-for file in log*.out; do
-    line=$(grep "CV error" "$file")
-    k_val=$(echo "$line" | grep -oP 'K=\K\d+')
-    cv_val=$(echo "$line" | awk -F": " '{print $2}')
-    echo -e "$k_val\t$cv_val"
-done | sort -n > cverrors.seed3
