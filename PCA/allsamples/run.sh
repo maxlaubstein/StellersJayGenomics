7e0ea4054c61150d8ae08f3e84f2510bfcb7e5a8
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2026-05-29 18:07:33 -0400

    Add run.sh for PCA analysis with plink

---
+++ PCA/allsamples/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+plink --vcf /media/maxlaubstein/data1/STJARangewideGenomics/vcfdata/Cyanocitta_LDPruned_Autosomal.vcf.gz  --const-fid --allow-extra-chr --allow-no-sex --set-missing-var-ids @:#  --pca  --out Cyanocitta_PCA
+
