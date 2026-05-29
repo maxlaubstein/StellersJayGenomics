2e2fb6ac0713aec2d721ccf7dab86168a90d9b51
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2026-05-29 17:22:50 -0400

    Add makeinput.sh for PLINK data processing

---
+++ admixture/nomeso/makeinput.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+plink --vcf /media/maxlaubstein/data1/STJARangewideGenomics/vcfdata/Cyanocitta_LDPruned_Autosomal_No_Mesoamerica.vcf.gz \
+    --const-fid \
+    --allow-extra-chr \
+    --allow-no-sex \
+    --set-missing-var-ids @:#  \
+    --make-bed  \
+    --out Cyanocitta_LDPruned_Autosomal_nomeso
+
+plink --bfile Cyanocitta_LDPruned_Autosomal_nomeso --const-fid --allow-extra-chr --allow-no-sex --recode 12 --out Cyanocitta_LDPruned_Autosomal_nomeso
+
