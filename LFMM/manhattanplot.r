50dc285400985d62b466138f947a4072778bb25d
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2026-06-03 18:19:52 -0400

    Create manhattanplot.r

---
+++ LFMM/manhattanplot.r
@@ -0,0 +1,32 @@
+library(ggplot2)
+library(dplyr)
+library(data.table)
+library(ggrastr)
+
+args <- commandArgs(trailingOnly = TRUE)
+data <- fread(args[1])
+data$scaffold <- data$chr <- sub(".*\\.", "", data$chr)
+data$log10p <- -log10(data$p_var)
+
+data <- data %>%
+  arrange(scaffold, pos) %>%
+  mutate(order = row_number())
+
+
+plot <- ggplot(data, aes(x=order, y=log10p)) +
+  geom_point_rast(size = 0.5, aes(color = as.factor(scaffold)), alpha = 0.5, raster.dpi = 1000) +
+  theme_minimal() +
+  theme(
+    legend.position = "none",
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.x = element_blank()
+  ) +
+  scale_color_manual(values = rep(c('gray40','black'), 165)) +
+  ylab("-log10(p)") +
+  xlab("SNPs Ordered Across Scaffolds") +
+  ylim(0,6)
+
+
+ggsave(paste0(args[1], "_manhattan.pdf"), plot = plot, width = 7, height = 2, device = "pdf")
